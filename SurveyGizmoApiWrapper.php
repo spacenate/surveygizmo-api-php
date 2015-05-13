699043afce2360411b028ddeab45b6209f6cb0cf
Nathan Sollenberger <nsollenberger@gmail.com>
2015-05-12 20:46:09 -0600

    Initial commit

---
+++ SurveyGizmoApiWrapper.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * SurveyGizmo RESTful Api Wrapper
+ *
+ * @package SurveyGizmoApiWrapper
+ * @version 0.1
+ * @author Nathan Sollenberger <nsollenberger@gmail.com>
+ */
+namespace spacenate;
+
+/**
+ * SurveyGizmoApiWrapper class
+ */
+class SurveyGizmoApiWrapper
+{
+
+	public function hello()
+	{
+		return "hello world!";
+	}
+}
+
